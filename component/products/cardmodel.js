473e00a6ed7744d99cc5b6a202b13cc1cfce754b
Ngô Thành Thông <ngothanhthongcode@gmail.com>
2022-04-12 23:57:03 +0700

    Update cardmodel.js

--- component/products/cardmodel.js
+++ component/products/cardmodel.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
 const cardSchema = new Schema({
-    _id: { type: ObjectId },
+    
     user_id:{ type: String },
     name: { type: String },
     price: { type: Number },
